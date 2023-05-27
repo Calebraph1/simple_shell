e183fd680873f1d95e7da2f088d4fbbfb5acec31
Calebraph1 <craphl16@gmail.com>
2023-05-26 18:39:45 -0700

    Simple shell

--- errors1.c
+++ errors1.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a str to an int
- * @s: the str to be converted
- * Return: 0 if no nums in str, converted num otherwise
- * -1 on error
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
  */
 int _erratoi(char *s)
 {
@@ -12,18 +12,18 @@ int _erratoi(char *s)
 	unsigned long int result = 0;
 
 	if (*s == '+')
-	s++; /* TODO:why does this make main return 255?*/
-	for (i = 0; s[i] != '\0'; i++)
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-	result *= 10;
-	result += (s[i] - '0');
-	if (result > INT_MAX)
-	return (-1);
-	}
-	else
-	return (-1);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
 	}
 	return (result);
 }
@@ -31,9 +31,9 @@ int _erratoi(char *s)
 /**
  * print_error - prints an error message
  * @info: the parameter & return info struct
- * @estr: str containing specified error type
- * Return: 0 if no nums in str, converted num otherwise
- * -1 on error
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
  */
 void print_error(info_t *info, char *estr)
 {
@@ -45,6 +45,7 @@ void print_error(info_t *info, char *estr)
 	_eputs(": ");
 	_eputs(estr);
 }
+
 /**
  * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
@@ -59,26 +60,28 @@ int print_d(int input, int fd)
 	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
-	__putchar = _eputchar;
+		__putchar = _eputchar;
 	if (input < 0)
 	{
-	_abs_ = -input;
-	count++;
+		_abs_ = -input;
+		__putchar('-');
+		count++;
 	}
 	else
-	_abs_ = input;
+		_abs_ = input;
 	current = _abs_;
 	for (i = 1000000000; i > 1; i /= 10)
 	{
-	if (_abs_ / i)
-	{
-	__putchar('0' + current / i);
-	count++;
-	}
-	current %= i;
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
 	}
 	__putchar('0' + current);
 	count++;
+
 	return (count);
 }
 
@@ -100,8 +103,9 @@ char *convert_number(long int num, int base, int flags)
 
 	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
-	n = -num;
-	sign = '-';
+		n = -num;
+		sign = '-';
+
 	}
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
 	ptr = &buffer[49];
@@ -113,7 +117,7 @@ char *convert_number(long int num, int base, int flags)
 	} while (n != 0);
 
 	if (sign)
-	*--ptr = sign;
+		*--ptr = sign;
 	return (ptr);
 }
 
@@ -128,9 +132,9 @@ void remove_comments(char *buf)
 	int i;
 
 	for (i = 0; buf[i] != '\0'; i++)
-	if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
-	{
-	buf[i] = '\0';
-	break;
-	}
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
 }
